d621ca75c870152cefb9cdccac10d3905f24bc07
Thinh Ha <havanthinh97@gmail.com>
2022-05-09 19:44:25 +0700

    Create Products, Product components.
    Create Products, Product components and add some stylings.

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React from 'react'
-
+import Products from './components/Products/Products'
 const App = () => {
   return (
-    <div>E-commerce</div>
+    <div><Products/></div>
   )
 }
 
---
+++ src/components/Products/Products.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import { Grid } from '@material-ui/core'
+import Product from './Product/Product'
+
+const products = [
+  { id: 1, name: 'Shoes', price: '1.050.000VNĐ', description: 'Running shoes' , image: 'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fd.line-scdn.net%2Flcp-prod-photo%2F20190717_74%2F1563311570896zsLXx_JPEG%2F818fMp-SaQL.jpg&f=1&nofb=1' },
+  { id: 2, name: 'Shirts', price: '299.000VNĐ', description: 'Running shirts', image: 'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Flive.staticflickr.com%2F7219%2F13928964272_b912842f6f.jpg&f=1&nofb=1' },
+  { id: 3, name: 'Pants', price: '399.000VNĐ', description: 'Running pants', image: 'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fi.ebayimg.com%2Fimages%2Fg%2FpZ4AAOSw7mhedW5v%2Fs-l1600.jpg&f=1&nofb=1' },
+]
+
+const Products = () => {
+  return (
+    <main>
+      <Grid container justify="center" spacing={4}>
+        {products.map((product) => (
+          <Grid item key={product.id} xs={12} sm={6} md={4} lg={3}>
+            <Product product={product} />
+          </Grid>
+        ))}
+      </Grid>
+    </main>
+  )
+}
+
+export default Products
