baefe89ba4417c95e8b315d17c70da41f260c55e
Thinh Ha <havanthinh97@gmail.com>
2022-05-10 14:01:08 +0700

    Fix typo.

--- src/components/Cart/CartItem/styles.js
+++ src/components/Cart/CartItem/styles.js
@@ -2,7 +2,7 @@ import { makeStyles } from '@material-ui/core/styles'
 
 export default makeStyles((theme) => ({
   media: {
-    height: '260',
+    height: '240px',
   },
   cardContent: {
     display: 'flex',
