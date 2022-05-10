df3d0125b2d21721f2a5760c38b73ade61079678
Thinh Ha <havanthinh97@gmail.com>
2022-05-10 12:28:36 +0700

    Use Routes to display comps according to path

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React, { useEffect, useState } from 'react'
 import { Products, Navbar, Cart } from './components'
 import { commerce } from './lib/commerce'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 const App = () => {
   const [products, setProducts] = useState([])
   const [cart, setCart] = useState({})
   const fetchProducts = async () => {
-    const { data } = await commerce.products.list();
-    
-    setProducts(data);
+    const { data } = await commerce.products.list()
+
+    setProducts(data)
   }
   const fetchCart = async () => {
     setCart(await commerce.cart.retrieve())
@@ -22,11 +23,15 @@ const App = () => {
     fetchCart()
   }, [])
   return (
-    <div>
-      <Navbar totalItems={cart.total_items}/>
-      <Products products={products} onAddToCart={handleAddToCart}/>
-      <Cart cart={cart}/>
+    <Router>
+      <div>
+        <Navbar totalItems={cart.total_items} />
+        <Routes>
+          <Route path="/" element={<Products products={products} onAddToCart={handleAddToCart} />}/>
+          <Route path="/cart" element={<Cart cart={cart}/>}/>
+        </Routes>
       </div>
+    </Router>
   )
 }
 
