d239191d33fbbec2b878f3ad23d3dc6039d5384c
Thinh Ha <havanthinh97@gmail.com>
2022-05-10 12:30:40 +0700

    Change Navbar to not show 'goToCart'.
    Change Navbar to not show 'goToCart' btn while at cart route.

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Link, useLocation } from 'react-router-dom'
 import {
   AppBar,
   Toolbar,
@@ -9,13 +10,20 @@ import {
 import useStyles from './styles'
 import { ShoppingCart } from '@material-ui/icons'
 import logo from '../../assets/Ecommerce.png'
-const Navbar = ({totalItems}) => {
+const Navbar = ({ totalItems }) => {
   const classes = useStyles()
+  const location = useLocation()
   return (
     <>
       <AppBar position="fixed" className="classes.appBar" color="inherit">
         <Toolbar>
-          <Typography variant="h6" className={classes.title} color="inherit">
+          <Typography
+            component={Link}
+            to="/"
+            variant="h6"
+            className={classes.title}
+            color="inherit"
+          >
             <img
               src={logo}
               alt="Commerce.js"
@@ -26,11 +34,18 @@ const Navbar = ({totalItems}) => {
           </Typography>
           <div className={classes.grow} />
           <div className={classes.button}>
-            <IconButton color="inherit" aria-label="cart">
-              <Badge badgeContent={totalItems} color="secondary">
-                <ShoppingCart />
-              </Badge>
-            </IconButton>
+            {location.pathname !== '/cart' && (
+              <IconButton
+                component={Link}
+                to="/cart"
+                color="inherit"
+                aria-label="cart"
+              >
+                <Badge badgeContent={totalItems} color="secondary">
+                  <ShoppingCart />
+                </Badge>
+              </IconButton>
+            )}
           </div>
         </Toolbar>
       </AppBar>
